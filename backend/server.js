3eed2b063e56b8823dd50be7139fcde0d25f8d86
Sandeep Rishi <rishisandeep8@gmail.com>
2024-09-15 05:44:16 +0530

    require to import

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
-const cors = require('cors');
+// const cors = require('cors');
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
